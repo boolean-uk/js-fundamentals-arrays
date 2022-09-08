1362c702304d2aed94a46118738aebb8ebc67753
George Roth <g@georgeroth.eu>
2022-09-08 12:47:35 +0100

    Sixth question

--- src/extension.js
+++ src/extension.js
@@ -23,7 +23,7 @@ colours.shift()
 keys.pop()
 
 // 6. Remove Jordan from the countries array
-
+countries.splice(1, 1)
 
 // 7. Remove the last fruit from the fruits array and store it in pear below
 const pear = undefined
