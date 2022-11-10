16621be6a9ac19ccc6c371a128e066bce19f31b9
dearshrewdwit <dearshrewdwit@gmail.com>
2022-11-10 10:45:58 +0000

    Update formatting

--- src/core.js
+++ src/core.js
@@ -1,12 +1,11 @@
 // Don't change this array directly on line 2
 const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 
-// 1. Using an index on the teachers array, change the value of the 
+// 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
 const fourthTeacher = undefined
 
 // 2. Replace the fifth teacher in the teachers array with Patrick
-
 
 // 3. Remove the last teacher from the array and save them in lastTeacher below
 const lastTeacher = undefined
@@ -16,17 +15,14 @@ const firstTeacher = undefined
 
 // 5. Add a teacher named Vanessa to the end of the teachers array
 
-
 // 6. Remove Ed from the teachers array
-
 
 // 7. Add a teacher named Sarah to the beginning of the teachers array
 
-
 // Don't change the code below this line
 module.exports = {
-    teachers,
-    fourthTeacher,
-    lastTeacher,
-    firstTeacher
+  teachers,
+  fourthTeacher,
+  lastTeacher,
+  firstTeacher
 }
