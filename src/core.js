45605787982d5a4b81f0be4621b7399215c8fefa
Kat <katdemey@outlook.com>
2022-11-10 11:15:14 +0000

    did q 1-4

--- src/core.js
+++ src/core.js
@@ -3,15 +3,20 @@ const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 
 // 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
-const fourthTeacher = undefined
+const fourthTeacher = teachers[3]
+console.log(fourthTeacher)
 
 // 2. Replace the fifth teacher in the teachers array with Patrick
+teachers [4] = 'Patrick'
+console.log(teachers)
 
 // 3. Remove the last teacher from the array and save them in lastTeacher below
-const lastTeacher = undefined
+const lastTeacher = teachers.pop()
+console.log(teachers)
 
 // 4. Remove the first teacher from the array and save them in firstTeacher below
-const firstTeacher = undefined
+const firstTeacher = names.shift()
+console.log(teachers)
 
 // 5. Add a teacher named Vanessa to the end of the teachers array
 
