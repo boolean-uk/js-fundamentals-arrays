3dc18009aa43e3c581160cdb4ae6f26e83d705d4
MaybeFreak <michaelpope97@gmail.com>
2022-11-10 14:22:02 +0100

    fixed a small mistake and added better logging for debugging

--- src/core.js
+++ src/core.js
@@ -4,32 +4,32 @@ const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 // 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
 const fourthTeacher = teachers[3]
-console.log(fourthTeacher)
+console.log('Fourth Teacher: ' + fourthTeacher)
 
 // 2. Replace the fifth teacher in the teachers array with Patrick
-teachers.splice(4, 1, 'Patrick')
-console.log(teachers)
+teachers[4] = 'Patrick'
+console.log('5th should be Patrick: ' + teachers)
 
 // 3. Remove the last teacher from the array and save them in lastTeacher below
-const lastTeacher = teachers[teachers.length - 1]
-console.log(lastTeacher)
-console.log(teachers)
+const lastTeacher = teachers.pop()
+console.log('Should log Shahzad: ' + lastTeacher)
+console.log('Shahzad should be removed: ' + teachers)
 
 // 4. Remove the first teacher from the array and save them in firstTeacher below
 const firstTeacher = teachers.shift()
-console.log(firstTeacher)
+console.log('Should log Nathan: ' + firstTeacher)
 
 // 5. Add a teacher named Vanessa to the end of the teachers array
-const nameTeacher = teachers.push('Vanessa')
-console.log(nameTeacher)
+teachers.push('Vanessa')
+console.log('Vanesssa should be at last index: ' + teachers)
 
 // 6. Remove Ed from the teachers array
 teachers.shift()
-console.log(teachers)
+console.log('Ed should be removed: ' + teachers)
 
 // 7. Add a teacher named Sarah to the beginning of the teachers array
 teachers.unshift('Sarah')
-console.log(teachers)
+console.log('Sarah should be at first index: ' + teachers)
 
 // Don't change the code below this line
 module.exports = {
