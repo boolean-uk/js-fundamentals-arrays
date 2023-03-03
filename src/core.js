57e3cb2e6eeb57fce11543b63fc4c03fbff5c308
Isabelle <isabel.tartarelli@live.it>
2023-03-03 12:36:42 +0000

    Modifing and correcting errors

--- src/core.js
+++ src/core.js
@@ -3,10 +3,12 @@ const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 
 // 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
-const fourthTeacher = teachers[4]
+const fourthTeacher = teachers[3]
 
 // 2. Replace the fifth teacher in the teachers array with Patrick
-teachers.splice(5,1, "Patrick")
+teachers.splice(4, 1, 'Patrick')
+console.log(teachers)
+
 // 3. Remove the last teacher from the array and save them in lastTeacher below
 const lastTeacher = teachers.pop()
 
@@ -15,14 +17,14 @@ const firstTeacher = teachers.shift()
 console.log(teachers)
 
 // 5. Add a teacher named Vanessa to the end of the teachers array
-const addTeacher = teachers.push("Vanessa");
+const addTeacher = teachers.push('Vanessa')
 
 // 6. Remove Ed from the teachers array
-teachers.splice(0, 1);
+teachers.splice(0, 1)
 //const edIndex = teachers.
 
 // 7. Add a teacher named Sarah to the beginning of the teachers array
-teachers.unshift("Sarah");
+teachers.unshift('Sarah')
 
 // Don't change the code below this line
 module.exports = {
