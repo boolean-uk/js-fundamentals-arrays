97a3ea659b611abcc2de9574e75a49403c0aa95f
joeri luteyn <joeriluteyn@gmail.com>
2023-04-05 15:56:33 +0200

    core assignment finished

--- src/core.js
+++ src/core.js
@@ -3,21 +3,34 @@ const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 
 // 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
-const fourthTeacher = undefined
+
+const fourthTeacher = teachers[3]
 
 // 2. Replace the fifth teacher in the teachers array with Patrick
 
+teachers[4] = 'Patrick'
+
 // 3. Remove the last teacher from the array and save them in lastTeacher below
-const lastTeacher = undefined
+
+const removeLastTeacher = teachers.pop()
+const lastTeacher = removeLastTeacher
 
 // 4. Remove the first teacher from the array and save them in firstTeacher below
-const firstTeacher = undefined
+
+const removeFirstTeacher = teachers.shift()
+const firstTeacher = removeFirstTeacher
 
 // 5. Add a teacher named Vanessa to the end of the teachers array
 
+teachers.push('Vanessa')
+
 // 6. Remove Ed from the teachers array
 
+teachers.shift()
+
 // 7. Add a teacher named Sarah to the beginning of the teachers array
+
+teachers.unshift('Sarah')
 
 // Don't change the code below this line
 module.exports = {
