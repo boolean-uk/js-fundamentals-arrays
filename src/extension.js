c4ba658998a5406dddc9083922341ac59a87a59e
ThanosPa <papanikolaoua7@gmail.com>
2023-04-05 16:39:18 +0300

    Thanasis Papanikolaou half extension

--- src/extension.js
+++ src/extension.js
@@ -8,13 +8,16 @@ const countries = ['Bolivia', 'Jordan', 'Greenland']
 const fruits = ['Apple', 'Orange', 'Pear']
 
 // 1. Add Fred to the names array
+names.push('Fred')
 
 // 2. Add 4 to the end of the numbers array
+numbers.push(4)
 
 // 3. Add Rio to the start of the cities array
+cities.unshift('Rio')
 
 // 4. Remove the first colour from the colours array
-
+colours.shift()
 // 5. Remove the last item from the keys array
 
 // 6. Remove Jordan from the countries array
