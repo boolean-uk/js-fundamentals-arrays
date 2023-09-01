d94c7ebb7efde2e9854ceb1b2b8564d4f32f1861
Chloe Zermatten <chloezermatten@gmail.com>
2023-09-01 15:18:40 +0200

    Chloe's commit

--- src/core.js
+++ src/core.js
@@ -1,34 +1,32 @@
 // Don't change this array directly on line 2
 const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 
-
 // 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
-const fourthTeacher = teachers[3];
-console.log(` 1: ${fourthTeacher}`);
+const fourthTeacher = teachers[3]
+console.log(` 1: ${fourthTeacher}`)
 
 // 2. Replace the fifth teacher in the teachers array with Patrick
 teachers[4] = 'Patrick'
-console.log(` 2: ${fourthTeacher}`);
+console.log(` 2: ${fourthTeacher}`)
 // 3. Remove the last teacher from the array and save them in lastTeacher below
 const lastTeacher = teachers.pop()
-console.log(` 3: ${lastTeacher}`);
+console.log(` 3: ${lastTeacher}`)
 // 4. Remove the first teacher from the array and save them in firstTeacher below
 const firstTeacher = teachers.shift()
-console.log(` 4: ${firstTeacher}`);
+console.log(` 4: ${firstTeacher}`)
 
 // 5. Add a teacher named Vanessa to the end of the teachers array
 teachers.push('Vanessa')
-console.log(` 5: ${teachers}`);
+console.log(` 5: ${teachers}`)
 
 // 6. Remove Ed from the teachers array
 teachers.shift(0, 1)
-console.log(` 6: ${teachers}`);
+console.log(` 6: ${teachers}`)
 
 // 7. Add a teacher named Sarah to the beginning of the teachers array
 teachers.unshift('Sarah')
-console.log(` 7: ${teachers}`);
-
+console.log(` 7: ${teachers}`)
 
 // Don't change the code below this line
 module.exports = {
--- src/extension.js
+++ src/extension.js
@@ -8,19 +8,30 @@ const countries = ['Bolivia', 'Jordan', 'Greenland']
 const fruits = ['Apple', 'Orange', 'Pear']
 
 // 1. Add Fred to the names array
-
+names.push('Fred')
+console.log(`1: ${names}`)
 // 2. Add 4 to the end of the numbers array
-
+numbers.push(4)
+console.log(`2: ${numbers}`)
 // 3. Add Rio to the start of the cities array
+cities.unshift('Rio')
+console.log(`3: ${cities}`)
 
 // 4. Remove the first colour from the colours array
+colours.shift()
+console.log(`4: ${colours}`)
 
 // 5. Remove the last item from the keys array
+keys.pop()
+console.log(`5: ${keys}`)
 
 // 6. Remove Jordan from the countries array
+countries.splice(1, 1)
+console.log(`6: ${countries}`)
 
 // 7. Remove the last fruit from the fruits array and store it in pear below
-const pear = undefined
+const pear = fruits.pop()
+console.log(`7: ${pear}`)
 
 // Do not change the code below
 module.exports = {
