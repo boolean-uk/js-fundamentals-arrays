b6aa125c89b490cbb501834f6c4cb9789f507fe1
Pierluigi <pierluigi.capirci89@gmail.com>
2023-09-01 16:33:20 +0100

    question number 5 solved

--- src/extension.js
+++ src/extension.js
@@ -16,7 +16,7 @@ cities.unshift('Rio')
 // 4. Remove the first colour from the colours array
 colours.shift()
 // 5. Remove the last item from the keys array
-
+keys.pop()
 // 6. Remove Jordan from the countries array
 
 // 7. Remove the last fruit from the fruits array and store it in pear below
