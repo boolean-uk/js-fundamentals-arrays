40551aed6292ea4b5cf812288b2eb8c7c793864f
ilham saleh <ilham2saleh@gmail.com>
2023-09-01 16:13:06 +0100

    extensions completed by ilham

--- src/extension.js
+++ src/extension.js
@@ -32,7 +32,7 @@ countries.splice(1, 1)
 console.log(countries)
 
 // 7. Remove the last fruit from the fruits array and store it in pear below
-const pear = fruits.splice(2, 1).slice()
+const pear = fruits.pop()
 console.log(pear)
 
 // Do not change the code below
