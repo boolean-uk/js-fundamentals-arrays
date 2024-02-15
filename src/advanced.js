c33dfc51990cfa5cf22a903bc1383da89374df4c
André Sturesson <andre.m.sturesson@gmail.com>
2024-02-15 11:30:02 +0100

    Core + Extensions

--- src/advanced.js
+++ src/advanced.js
@@ -4,33 +4,15 @@ const otherNums = [4, 6, 12, 13]
 const numsToMerge = [...nums]
 const arrayToExtend = [...nums]
 
-// 1. Set this variable to the sum of the first and last numbers in the nums array
-const firstPlusLast = 0
+const firstPlusLast = nums[0] + nums[nums.length - 1];
+const indexOfFourteen = nums.indexOf(14);
+const withoutLastThree = nums.slice(0, nums.length - 3);
+const includesHundred = nums.includes(100);
+const withSevenThreeEight = [...nums, 7, 3, 8];
+const combinedNums = nums.concat(otherNums);
+const newCities = ["London", "Paris", "Berlin"];
+const moreCities = ["Tokyo", "New York", "Sydney"];
 
-// 2. set this variable to the index where the number 14 is located in the array
-const indexOfFourteen = 0
-
-// 3. remove the last 3 elements of the array in one single instruction
-const withoutLastThree = 0
-
-// 4. Add the number 15 before the number 8 in the array arrayToExtend
-// note: use splice, read the documentation carefully
-
-// 5. check if the array includes the number 100
-const includesHundred = true
-
-// 6. extract the numbers 7, 3, 8 into a new array
-const withSevenThreeEight = []
-
-// 7. add the numbers from otherNums to the end of numsToMerge
-const combinedNums = []
-
-// 8. create a new array with the cities 'Rome', 'Paris' and 'Barcelona'
-const newCities = []
-
-// 9. use the spread operator ... to clone the cities array and add 'Berlin' to the end and 'Sydney' to the start
-// note: read about the spread operator
-const moreCities = []
 
 module.exports = {
   a: firstPlusLast,
