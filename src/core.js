5a679d3ea6c310fc60d800ab5aea2c20f3c67810
Santhia <santhia.shanmukarasan@gmail.com>
2024-02-16 13:21:33 +0100

    All tasks completed

--- src/core.js
+++ src/core.js
@@ -3,21 +3,28 @@ const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 
 // 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
-const fourthTeacher = undefined
+const fourthTeacher = teachers[3];
 
 // 2. Replace the fifth teacher in the teachers array with 'Patrick'
+teachers[4] = 'Patrick';
 
 // 3. Remove the last teacher from the array and save them in lastTeacher below
-const lastTeacher = undefined
+//pop() removes the last element from the array teachers.
+const lastTeacher = teachers.pop();
 
 // 4. Remove the first teacher from the array and save them in firstTeacher below
-const firstTeacher = undefined
+//The shift() method is used to remove the first element from an array and returns that removed element.
+const firstTeacher = teachers.shift();
 
 // 5. Add a teacher named 'Vanessa' to the end of the teachers array
+const newTeacher = teachers.push('Vanessa');
 
 // 6. Remove 'Ed' from the teachers array
+const removeTeacher = teachers.splice(teachers.indexOf('Ed'),1);
 
 // 7. Add a teacher named 'Sarah' to the beginning of the teachers array
+const newTeacher2 = teachers.unshift('Sarah')
+
 
 // Don't change the code below this line
 module.exports = {
