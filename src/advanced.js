9d832a0e36bf6a0ed30df042f1504b28d68f1af3
HomoNOVISCoding <mama.leye@gmail.com>
2024-02-16 09:36:45 +0000

    done

--- src/advanced.js
+++ src/advanced.js
@@ -5,19 +5,19 @@ const numsToMerge = [...nums]
 const arrayToExtend = [...nums]
 
 // 1. Set this variable to the sum of the first and last numbers in the nums array
-const firstPlusLast = 0
+const firstPlusLast = nums.at(0) + nums.at(8)
 
 // 2. set this variable to the index where the number 14 is located in the array
-const indexOfFourteen = 0
+const indexOfFourteen = nums.indexOf(14)
 
 // 3. remove the last 3 elements of the array in one single instruction
-const withoutLastThree = 0
+const withoutLastThree = nums.splice(6, 3)
 
 // 4. Add the number 15 before the number 8 in the array arrayToExtend
 // note: use splice, read the documentation carefully
-
+arrayToExtend.splice(2, 1, 15)
 // 5. check if the array includes the number 100
-const includesHundred = true
+const includesHundred = nums.includes(100)
 
 // 6. extract the numbers 7, 3, 8 into a new array
 const withSevenThreeEight = []
